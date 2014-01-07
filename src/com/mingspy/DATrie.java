3e695c6661cf13dedd36d7c078c05e016739c7c2
xiuleili@sphu-inc.com <xiuleili@sohu-inc.com>
2014-01-07 18:16:35 +0800

    DAT接口

---
+++ src/com/mingspy/DATrie.java
@@ -0,0 +1,9 @@
+package com.mingspy;
+
+public abstract interface DATrie<K,V> {
+	public boolean add(K key, V value);
+	public boolean remove(K key);
+	public boolean containsPrefix(K key);
+	public boolean unserialize(String path);
+	public boolean serialize(String path);
+}
---
+++ src/com/mingspy/modles/TrieItem.java
@@ -0,0 +1,26 @@
+package com.mingspy.modles;
+
+public class TrieItem {
+	private int base;
+	private int check;
+	private Object value;
+	public int getBase() {
+		return base;
+	}
+	public void setBase(int base) {
+		this.base = base;
+	}
+	public int getCheck() {
+		return check;
+	}
+	public void setCheck(int check) {
+		this.check = check;
+	}
+	public Object getValue() {
+		return value;
+	}
+	public void setValue(Object value) {
+		this.value = value;
+	}
+	
+}
